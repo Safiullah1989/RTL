b9ca4159657558a7a2e28f0adb535261eb960618
Safiullah <91043698+Safiullah1989@users.noreply.github.com>
2021-10-31 09:03:38 +0600

    Login Added

--- RTLSourceCode/RTLConsole/Program.cs
+++ RTLSourceCode/RTLConsole/Program.cs
@@ -6,7 +6,35 @@ namespace RTLConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+        private static void Login()
+        {
+            Console.Write("Enter User Name:");
+            var userName = Console.ReadLine();
+            Console.WriteLine(userName);
+
+            Console.Write("Enter Password:");
+            var password = string.Empty;
+            ConsoleKey key;
+            do
+            {
+                var keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+
+                if (key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    password = password[0..^1];
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    Console.Write("*");
+                    password += keyInfo.KeyChar;
+                }
+            } while (key != ConsoleKey.Enter);
+            Console.WriteLine();
+            Console.WriteLine(password);
+
         }
     }
 }
